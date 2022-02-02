199bd3f530db85b56140dd953befce6297c32cd7
bejaouiHajer <hejertrt2@gmail.com>
2022-02-02 12:16:05 +0100

    adding a user component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import AddUser from "./components/Users/AddUser";
 
-import './App.css';
+
 
 function App() {
   return (
     <div>
+     <AddUser />
     </div>
   );
 }
